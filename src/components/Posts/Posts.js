1535b34f5e7df58cd3c805b5a696a4bb0df63516
Max Ivanov <waxivanov@gmail.com>
2021-11-07 17:39:25 +0200

    if render

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -7,7 +7,16 @@ function Posts({ posts, addNewPost, deletePost }) {
     <div className={style.posts}>
       <h1 className={style.postsTitle}>{posts.title}</h1>
       <PostCreator addNewPost={addNewPost} />
-      <List items={posts.data} deletePost={deletePost} />
+
+      {posts.data.length !== 0 ? (
+        <List items={posts.data} deletePost={deletePost} />
+      ) : (
+        <div>
+          <h2 style={{ textAlign: "center", padding: "20px 0" }}>
+            Post List Is Empty
+          </h2>
+        </div>
+      )}
     </div>
   );
 }
