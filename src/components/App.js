ebe2e6c92396a149dd0833e5b810d9bbba719a61
ASTOUND\m.ivanov <m.ivanov@astoundcommerce.com>
2022-03-17 16:51:16 +0200

    post lists

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import PostList from "./PostList";
 import styles from "./App.module.css";
 
 export default function App() {
-  const [posts, setPosts] = useState([
+  const [frontend, setFrontend] = useState([
     {
       id: 1,
       title: "HTML",
@@ -21,9 +21,23 @@ export default function App() {
     },
   ]);
 
+  const [backend, setBackend] = useState([
+    {
+      id: 1,
+      title: "NodeJS",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      id: 2,
+      title: "Python",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, perspiciatis quia nihil animi illum cum neque nesciunt maiores ducimus nam!",
+    },
+  ]);
+
   return (
     <div className={styles.app}>
-      <PostList posts={posts} title={"Frontend"} />
+      <PostList posts={frontend} title={"Frontend"} />
+      <PostList posts={backend} title={"Backend"} />
     </div>
   );
 }
